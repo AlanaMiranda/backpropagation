06d687090ffc1216c68e969501e7ef53c9fcb0d5
Joaquim Viana <joaquimviana@JViana-Mac-2024.local>
2024-05-07 00:18:55 -0300

    #

--- backpropagation/camadas.py
+++ backpropagation/camadas.py
@@ -2,23 +2,17 @@ import numpy as np
 
 
 # INICIALIZACAO DOS PESOS
-# Recebe o numero de neuronios na  entrada e numero de neuronios na saida
-# Gera uma matriz de pesos com numeros pequenos aleatorios de -9 a 9
-# Divide por 1000 para obetr nrs mais pequenos ainda\
-def pesos (nr_neurons_entrada: int, nr_neurons_saida:int) -> np.ndarray:
-    pesos = np.random.randint(-9,9, size=(nr_neurons_entrada, nr_neurons_saida))/1000
-
-    return pesos
+def pesos ():
+    '''CODE HERE'''
+    pass
 
 
 
 # INICIALIZACAO DOS BIASES
-# Recebe numero de neuronios da camada seguinte
-# Gera um vetor coluna de zeros
-def biases (nr_neurons_saida:int) -> np.ndarray:
-    bias = np.zeros((nr_neurons_saida,1))
-
-    return bias
+def biases ():
+    '''CODE HERE'''
+    pass
+ 
 
 
 
@@ -43,5 +37,5 @@ def custo_emq (saida_atual:np.ndarray, saida_real:np.ndarray) -> np.ndarray:
 # RETROPROPAGACAO
 # Faz backward entre as camadas
 def retropropagacao():
-    # CODE HERE
+    '''CODE HERE'''
     pass
