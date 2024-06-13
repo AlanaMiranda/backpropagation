4f896cef9f13231d347c79082edc26cd24fb5395
AlanaMiranda <74725375+AlanaMiranda@users.noreply.github.com>
2024-06-12 22:07:49 -0300

    Update camadas.py

--- backpropagation/camadas.py
+++ backpropagation/camadas.py
@@ -14,13 +14,13 @@ from .funcoes import deriv_relu, deriv_sigmoide, deriv_tanh, deriv_custo_emq, at
 # n_saidas: número de neurônios na saída
 def pesos (n_entradas:int, n_saidas:int, low=-1, high=1) -> np.ndarray:
     return np.random.uniform(low, high, (n_entradas, n_saidas))
-
+#gera uma matriz de pesos com valores aleatórios dentro do intervalo especificado por low e high
 
 
 # INICIALIZACAO DOS BIASES
 def biases (n_entradas:int) -> np.ndarray:
     return np.zeros((n_entradas, 1))
- 
+ #cria uma matriz de zeros com dimensões n_entradas x 1, onde cada entrada (ou neurônio) recebe um bias inicial de zero.
 
 
 # Retorna uma matriz com a soma ponderada entre pesos e biases
@@ -30,21 +30,28 @@ def soma_ponderada (entrada: np.ndarray, pesos: np.ndarray, bias:np.ndarray) -> 
 
 
 # PROPAGACAO DIRETA
+
 # Faz o feedforward entre as camadas
 def propagacao_direta(lote_x_treino: np.ndarray, lote_y_treino:np.ndarray,
                       W2:np.ndarray, B2:np.ndarray, f_ativacao:str,
                       W1:np.ndarray, B1:np.ndarray
                       )->tuple[np.ndarray, np.ndarray, np.ndarray]:
  
-    # Propagação direta p/camada oculta          
+    # Propagação direta p/camada oculta
+    # Calcula a soma ponderada das entradas lote_x_treino usando os pesos W2 e bias B2.
+    # Aplica a função de ativação especificada (f_ativacao) na soma ponderada para obter a ativação da camada escondida.
     soma_oculta = soma_ponderada(lote_x_treino, W2, B2)
     ativacao_oculta = ativacao(soma_oculta, f_ativacao)  # Ativação na camada escondida
 
     # Propagação direta p/camada saida 
+    # Calcula a soma ponderada das ativações ocultas usando os pesos W1 e bias B1.
+    # Aplica a função de ativação sigmoide na soma ponderada para obter a ativação da camada de saída.
     soma_saida = soma_ponderada(ativacao_oculta, W1, B1)
     ativacao_saida = ativacao(soma_saida, 'sigmoide')  # Ativação na camada de saída
     
     # Cálculo de erro na saída
+    # Utiliza a função de custo do erro quadrático médio (custo_emq), 
+    # comparando a ativação da camada de saída com as saídas esperadas lote_y_treino.
     erro_atual = custo_emq(ativacao_saida,lote_y_treino)
     
     return ativacao_oculta, ativacao_saida, erro_atual
